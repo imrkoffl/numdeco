34c0ac029469a78f8d88a405c6f18997dd9a62d4
Rishi Kumar <rishikumaroffl@gmail.com>
2021-10-31 12:11:56 +0530

    two lib
    deco
    decoco

--- numdeco.py
+++ numdeco.py
@@ -8,21 +8,37 @@ def deco(k=0):
     try :
         if k > 999999999999:
             print(str(k)[:-12]+'T '+str(k)[-12:-9]+'B '+str(k)[-9:-6]+'M '+str(k)[-6:-3]+'K '+str(k)[-3:])
-            print(str(k)[:-12]+','+str(k)[-12:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
         elif k > 999999999:
             print(str(k)[:-9]+'B '+str(k)[-9:-6]+'M '+str(k)[-6:-3]+'K '+str(k)[-3:])
-            print(str(k)[:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
         elif k > 999999:
             print(str(k)[:-6]+'M '+str(k)[-6:-3]+'K '+str(k)[-3:])
-            print(str(k)[:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
         elif k > 999:
             print(str(k)[:-3]+'K '+str(k)[-3:])
-            print(str(k)[:-3]+','+str(k)[-3:])
         elif k > 99:
             print(str(k)[:-3]+'0.'+str(k)[-3]+'K')
-            print(str(k)[:-3]+'0.'+str(k)[-3:])
         elif k > 9:
             print(str(k)[:-2]+'0.0'+str(k)[-2]+'K')
+        else:
+            print(str(k))
+
+    except :
+        print('Error!! Enter only numbers')
+
+
+def decoco(k=0):
+    
+    try :
+        if k > 999999999999:
+            print(str(k)[:-12]+','+str(k)[-12:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
+        elif k > 999999999:
+            print(str(k)[:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
+        elif k > 999999:
+            print(str(k)[:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
+        elif k > 999:
+            print(str(k)[:-3]+','+str(k)[-3:])
+        elif k > 99:
+            print(str(k)[:-3]+'0.'+str(k)[-3:])
+        elif k > 9:
             print(str(k)[:-2]+'0.0'+str(k)[-2:])
         else:
             print(str(k))
