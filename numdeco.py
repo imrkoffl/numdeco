e9c55af1765485bb197bc4ea34720d21a6ed625c
Rishi Kumar <rishikumaroffl@gmail.com>
2021-10-31 15:38:02 +0530

    with indian version
    #video views using deco
    #video views using decoco
    
    #video views using indeco
    #video views using indecoco

--- numdeco.py
+++ numdeco.py
@@ -4,7 +4,7 @@ Author : Rishi Kumar S
 '''
 
 def deco(k=0):
-    
+    # deco with suffix T(trillion), B(billion), M(million), K
     try :
         if k > 999999999999:
             print(str(k)[:-12]+'T '+str(k)[-12:-9]+'B '+str(k)[-9:-6]+'M '+str(k)[-6:-3]+'K '+str(k)[-3:])
@@ -26,7 +26,7 @@ def deco(k=0):
 
 
 def decoco(k=0):
-    
+    # decoco with suffix Commas(,)
     try :
         if k > 999999999999:
             print(str(k)[:-12]+','+str(k)[-12:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
@@ -34,6 +34,50 @@ def decoco(k=0):
             print(str(k)[:-9]+','+str(k)[-9:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
         elif k > 999999:
             print(str(k)[:-6]+','+str(k)[-6:-3]+','+str(k)[-3:])
+        elif k > 999:
+            print(str(k)[:-3]+','+str(k)[-3:])
+        elif k > 99:
+            print(str(k)[:-3]+'0.'+str(k)[-3:])
+        elif k > 9:
+            print(str(k)[:-2]+'0.0'+str(k)[-2:])
+        else:
+            print(str(k))
+
+    except :
+        print('Error!! Enter only numbers')
+
+# Indian version
+
+def indeco(k=0):
+    # deco with suffix C(Crore), L(Lakhs), T(Thousand)
+    try :
+        if k > 9999999:
+            print(str(k)[:-7]+'C '+str(k)[-7:-5]+'L '+str(k)[-5:-3]+'T '+str(k)[-3:])
+        elif k > 999999:
+            print(str(k)[:-5]+'L '+str(k)[-5:-3]+'T '+str(k)[-3:])
+        elif k > 99999:
+            print(str(k)[:-5]+'L '+str(k)[-5:-3]+'T '+str(k)[-3:])
+        elif k > 999:
+            print(str(k)[:-3]+'T '+str(k)[-3:])
+        elif k > 99:
+            print(str(k)[:-3]+'0.'+str(k)[-3]+'T')
+        elif k > 9:
+            print(str(k)[:-2]+'0.0'+str(k)[-2]+'T')
+        else:
+            print(str(k))
+
+    except :
+        print('Error!! Enter only numbers')
+        
+def indecoco(k=0):
+    # indecoco with suffix Commas(,)
+    try :
+        if k > 9999999:
+            print(str(k)[:-7]+','+str(k)[-7:-5]+','+str(k)[-5:-3]+','+str(k)[-3:])
+        elif k > 999999:
+            print(str(k)[:-5]+','+str(k)[-5:-3]+','+str(k)[-3:])
+        elif k > 99999:
+            print(str(k)[:-5]+','+str(k)[-5:-3]+','+str(k)[-3:])
         elif k > 999:
             print(str(k)[:-3]+','+str(k)[-3:])
         elif k > 99:
